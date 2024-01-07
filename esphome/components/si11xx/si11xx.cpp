bed8b998f9b68e6c2044a573ef1bff6617fabaab
psim1 <paul@garden.com.au>
2024-01-07 12:06:54 +1000

    Update si11xx.cpp

--- esphome/components/si11xx/si11xx.cpp
+++ esphome/components/si11xx/si11xx.cpp
@@ -174,7 +174,7 @@ uint8_t SI11xComponent::read_value_(uint8_t reg) {
   return data;
 }
 
-uint16_t SI11xComponent::read_value16_(uint8_t reg) {
+/*uint16_t SI11xComponent::read_value16_(uint8_t reg) {
   uint16_t data;
   if (!this->read_bytes(reg, reinterpret_cast<uint8_t *>(&data), 2)) {
     this->error_code_ = READ_FAILED;
@@ -183,6 +183,19 @@ uint16_t SI11xComponent::read_value16_(uint8_t reg) {
   }
   delay(SI11X_DELAY);
   return data;
+}*/
+
+uint16_t SI11xComponent::read_value16_(uint8_t reg) {
+  uint16_t data;
+  if (!this->read_bytes(reg, reinterpret_cast<uint8_t *>(&data), 2)) {
+    this->error_code_ = READ_FAILED;
+    this->mark_failed();
+    return 0;
+  }
+  delay(SI11X_DELAY);
+  uint8_t msb = data & 0x00FF;
+  uint8_t lsb = (data & 0xFF00) >> 8;
+  return (msb * 256) + lsb;
 }
 
 // Set any register value and handle errors
